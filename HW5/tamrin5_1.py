a2893e0f617d6c89ab60ed429bd143802c1d80c4
samira <samiramohammadian3@gmail.com>
2021-11-05 21:06:38 +0330

    debugging

--- HW5/tamrin5_1.py
+++ HW5/tamrin5_1.py
@@ -2,6 +2,7 @@ from os import linesep
 
 
 def show_menu():
+    print("----------------------------------------------------")
     print(" Welcome to my store: ")
     print(" 1.add ")
     print(" 2.edit ")
@@ -10,45 +11,52 @@ def show_menu():
     print(" 5.search ")
     print(" 6.buy ")
     print(" 7.exit ")
+    print("----------------------------------------------------")
 
 
-kala=[]
+Products=[]
 
 def load_data_from_datastore():
     f = open('datastore.csv', 'r')
     for line in f:
         info = line[:-1].split(',')
         new_dict = {'id': info[0], 'Name': info[1], 'Price': info[2], 'Stock': info[3]}
-        kala.append(new_dict)
+        Products.append(new_dict)
     
+def show_List():
+    print('id','\t\t'  ,  'Name','\t\t'  ,  'Price' ,'\t\t'  ,  'Stock')
+    for product in Products:
+        print(product['id'], '\t\t', product['Name'], '\t\t', product['Price'], '\t\t', product['Stock'])
 
 
 def add(): 
     while(True):
-        c=True
-        if c==True:
-            new_kala={'id': int(input("id kala ra vared konid.")) , 'Name': input("esm kala ra vared konid.") , 'Price': int(input("gheymate kala ra vared konid.")) , 'Stock' :int(input("tedad kala ra vared konid."))}
-
-            if new_kala not in kala :
-                kala.append(new_kala)
+        chiocess=True
+        if chiocess==True:
+            show_List()
+        
+            productId = int(input('enter Id of product : '))
+            for product in Products:
+                if productId == int(product['id']):
+                    print("product is existed ... ")
+                    break
             else:
-                print("kala mojod ast.")
+                productName = input('enter name of product : ')
+                productPrice = int(input('enter price of product : '))
+                productCount = int(input('enter stock of product : '))
+                Products.append({'id':productId,'Name':productName,'Price':productPrice,'Stock':productCount})
 
-        c=False
-        tekrar=(input("kalaye digari mikhahid vared kkonid (y/n) :"))
+        chiocess=False
+        tekrar=(input("Do you want to buy another product? (y/n) :"))
         if tekrar=="n":
             break
         elif tekrar=="y":
-            c=True
+            chiocess=True
         else :
-            print("gozine dorosti entekhab nakrdid.")
+            print("Wrong option..... try aggain")
             break
             
 
-def show_List():
-    print('id  ,  Name  ,  Price  ,  Stock')
-    for product in kala:
-        print(product['id'], '  ', product['Name'], '  ', product['Price'], '  ', product['Stock'])
 
 
 
@@ -56,45 +64,41 @@ def show_List():
 def edit():
     show_List()
     x=int(input())
-    for kal in kala:
-        if kal['id']==x:
+    for pr in Products:
+       if pr['id']==x:
 
-            kal['Name']=input('enter your Name :')
-            kal['Price']=input('enter your Price :')
-            kal['Stock']=input('enter your Stock :')
+            pr['Name']=input('enter your Name :')
+            pr['Price']=input('enter your Price :')
+            pr['Stock']=input('enter your Stock :')
 
 
 def delete():
     show_List()
-    x=int(input("id kala ra vared konid :"))
-    for kal in kala:
-        if kal['id']==x:
-            kala.remove(kal)
+    x=int(input("Enter the product id :"))
+    for pro in Products:
+        if pro['id']==x:
+            Products.remove(pro)
             
 
 
 
-
-
-
-
 def search():
-    name = input('name kala ra vared konid : ')
+    name = input('Enter the product name : ')
     k = 0
     found = False
-    for i in kala:
+    for i in Products:
         if i['Name'] == name:
             found = True
             break
         else:
             k += 1
     if found==True:
-        print(' natije josto joye shoma :', 
-              '\n id : ', kala[k]['id'],
-              '\nPrice: ', kala[k]['Price'],
-              '\nStock: ', kala[k]['Stock'])
+        print('Your search result :', 
+              '\n id : ', Products[k]['id'],
+              '\nPrice: ', Products[k]['Price'],
+              '\nStock: ', Products[k]['Stock'])
     else:
-        print('Dorost vared nakardid!')
+        print('You did not select the correct option!')
 
 
 def buy():
@@ -103,39 +107,47 @@ def buy():
     while True:
         
         if p==True:
-            x=input("esm mahsol ra vred kkonid: \n")
+            show_List()
+            x=input("Enter the product name : \n")
             
-            for i in kala:
-                if x in i["Name"]:
+            for i in Products:
+                if x == i["Name"]:
                     arr.append(i)
+                    i['Stock'] = int (i['Stock']) - 1
+                    break
+            else :
+                print ("Product not available. \n")
+                break
+                    
 
-        answer=input("kharid digari darid? (y/n) :")  
+
+        answer=input("Do you have another purchase?? (y/n) :")  
         if answer== "y" :
             p=True
         elif answer == "n":
             p=False  
             break
         else:
-            print("gozine dorost ra vared nakardid ")
+            print("You did not select the correct option !")
 
-
-    
     for i in arr:
         print( i["Name"],"\t",i["Price"],"\t",i["Stock"], "\n")
             
+
+
 def exit():
     f = open('datastore.csv', 'w')
     
-    for i in kala:
+    for i in Products:
 
-         f.write(i["id"]+","+i["Name"]+","+i["Price"]+","+i["Stock"]+"\n")
+        f.write(str(i["id"])+","+str(i["Name"])+","+str(i["Price"])+","+str(i["Stock"])+"\n")
+
     f.close()
 
-
+load_data_from_datastore()
 while True:
     show_menu()
     choice = int (input("please choose from menu : \n"))
-    load_data_from_datastore()
     if choice == 1:
         add()
     elif choice == 2:
